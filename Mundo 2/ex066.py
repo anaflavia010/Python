85bc5a6cc2bc3a22e3da918caf6ea7f03f98012d
Ana Flávia Motta dos Reis <reisanaflavia59@gmail.com>
2025-08-22 16:22:24 -0300

    atualizado

---
+++ Mundo 2/ex066.py
@@ -0,0 +1,8 @@
+cont = soma = 0
+while True:
+    n = int(input('Digite um número [999 - Finaliza o programa]: '))
+    if n == 999:
+        break
+    soma += n
+    cont += 1
+print(f'Você digitou {cont} números e a soma deles foi {soma}')
