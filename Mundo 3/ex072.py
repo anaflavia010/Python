e9c0acb71d998fd9a09912d277947149b67bc974
Ana Flávia Motta dos Reis <reisanaflavia59@gmail.com>
2025-08-25 14:58:46 -0300

    atualizado

---
+++ Mundo 3/ex072.py
@@ -0,0 +1,10 @@
+num = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez',
+       'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+while True:
+    while True:
+        n = int(input('Digite um número entre 0 e 20: '))
+        if 0 <= n <= 20:
+            break
+        print('Tente novamente! ', end='')
+    print(f'Você digitou o número {num[n]}')
+    break
