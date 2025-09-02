2f8522a41b2bb01e15259c7c4491b81a5bde0de3
Ana Flávia Motta dos Reis <reisanaflavia59@gmail.com>
2025-09-02 12:50:40 -0300

    atualizado

---
+++ Mundo 3/ex097.py
@@ -0,0 +1,9 @@
+def escreva(msg):
+    print('~' * (len(msg) + 2))
+    print(msg)
+    print('~' * (len(msg) + 2 ))
+
+
+escreva(' Gustavo Guanabara')
+escreva(' Curso de Python no YouTube')
+escreva(' CeV')
