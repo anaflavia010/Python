a849162fd57d28a4e9bf1e5d1cd402aaea71b60c
Ana Flávia Motta dos Reis <reisanaflavia59@gmail.com>
2025-09-05 14:47:38 -0300

    atualizado

---
+++ Mundo 3/ex009/teste.py
@@ -0,0 +1,7 @@
+import moeda
+
+preço = float(input('Digite o preço: '))
+print(f'a metade de {moeda.moeda(preço)} é {moeda.metade(preço, True)}')
+print(f'O dobro de {moeda.moeda(preço)} é {moeda.dobro(preço, True)}')
+print(f'Aumentando 10%, temos {moeda.aumentar(preço, 10, True)}')
+print(f'Diminuindo 13%, temos {moeda.diminuir(preço, 13, True)}')
